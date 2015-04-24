9e9fc93d7f3531e55d28b8c263d3f5192fcb209a
David Sungaila <david.sungaila@arcor.de>
2015-04-24 19:49:18 +0200

    Reguläre Ausdrücke für Twee-Links, -Macros und mehr eingebaut. Können mit Konsole getestet werden.

--- Twee2Z/Console/Program.cs
+++ Twee2Z/Console/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Twee2Z.Lexer;
 
 namespace Twee2Z.Console
 {
@@ -11,7 +13,32 @@ namespace Twee2Z.Console
         static void Main(string[] args)
         {
             System.Console.WriteLine("Welcome to Twee2Z");
-            System.Console.ReadKey();
+            System.Console.WriteLine("Try tokens like '<<display Page>>', '[[Place]]' or '<html><br></html>'");
+            System.Console.WriteLine("");
+            while (true)
+            {
+                System.Console.Write("Please enter a token: ");
+                string input = System.Console.ReadLine();
+
+                if (Regex.IsMatch(input, Pattern.Link))
+                    System.Console.WriteLine("Link\n");
+                else if (Regex.IsMatch(input, Pattern.Macro))
+                    System.Console.WriteLine("Macro\n");
+                else if (Regex.IsMatch(input, Pattern.Image))
+                    System.Console.WriteLine("Image\n");
+                else if (Regex.IsMatch(input, Pattern.HtmlBlock))
+                    System.Console.WriteLine("HtmlBlock\n");
+                else if (Regex.IsMatch(input, Pattern.Html))
+                    System.Console.WriteLine("Html\n");
+                else if (Regex.IsMatch(input, Pattern.InlineStyle))
+                    System.Console.WriteLine("InlineStyle\n");
+                else if (Regex.IsMatch(input, Pattern.Mono))
+                    System.Console.WriteLine("Mono\n");
+                else if (Regex.IsMatch(input, Pattern.Comment))
+                    System.Console.WriteLine("Comment\n");
+                else
+                    System.Console.WriteLine("Unknown token\n");
+            }
         }
     }
 }
---
+++ Twee2Z/Lexer/Pattern.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twee2Z.Lexer
+{
+    public static class Pattern
+    {
+        private const string UNQUOTED = @"(?=(?:[^""'\\]*(?:\\.|'(?:[^'\\]*\\.)*[^'\\]*'|""(?:[^""\\]*\\.)*[^""\\]*""))*[^'""]*$)";
+        private const string LINK = @"\[\[([^\|]*?)(?:\|(.*?))?\](\[.*?\])?\]";
+        private const string MACRO = @"<<([^>\s]+)(?:\s*)((?:\\.|'(?:[^'\\]*\\.)*[^'\\]*'|""(?:[^""\\]*\\.)*[^""\\]*""|[^'""\\>]|>(?!>))*)>>";
+        private const string IMAGE = @"\[([<]?)(>?)img\[(?:([^\|\]]+)\|)?([^\[\]\|]+)\](?:\[([^\]]*)\]?)?(\])";
+        private const string HTML_BLOCK = @"<html>((?:.|\n)*?)</html>";
+        private const string HTML = @"<(?:\/?([\w\-]+)(?:(\s+[\w\-]+(?:\s*=\s*(?:\"".*?\""|'.*?'|[^'\"">\s]+))?)+\s*|\s*)\/?)>";
+        private const string INLINE = "@@";
+        private const string INLINE_STYLE_PROP = @"((?:([^\(@]+)\(([^\)]+)(?:\):))|(?:([^:@]+):([^;@]+);)|(?:(\.[^\.;@]+);))+";
+        private const string MONO = @"^\{\{\{\n(?:(?:^[^\n]*\n)+?)(?:^\}\}\}$\n?)|\{\{\{((?:.|\n)*?)\}\}\}";
+        private const string COMMENT = @"/%((?:.|\n)*?)%/";
+        private const string MACRO_PARAMS_VAR = @"(\$[\w_\.]*[a-zA-Z_\.]+[\w_\.]*)";
+        private const string MACRO_PARAMS_FUNC = @"([\w\d_\.]+\((.*?)\))";
+        private const string MACRO_PARAMS = @"(?:(""(?:[^\\""]|\\.)*""|\'(?:[^\\\']|\\.)*\'|(?:\[\[(?:[^\]]*)\]\]))" +
+                    @"|\b(\-?\d+\.?(?:[eE][+\-]?\d+)?|NaN)\b" +
+                    @"|(true|false|null|undefined)" +
+                    "|" + MACRO_PARAMS_VAR + ")";
+        private const string IMAGE_FILENAME = @"[^\""']+\.(?:jpe?g|a?png|gif|bmp|webp|svg)";
+        private const string EXTERNAL_IMAGE_URL = @"\s*['\""]?(" + IMAGE_FILENAME + @")['\""]?\s*";
+        private const string HTML_IMAGE = @"src\s*=" + EXTERNAL_IMAGE_URL;
+        private const string CSS_IMAGE = @"url\s*\(" + EXTERNAL_IMAGE_URL + @"\)";
+
+        public static string Unquoted
+        {
+            get { return UNQUOTED; }
+        }
+        public static string Link
+        {
+            get { return LINK; }
+        }
+        public static string Macro
+        {
+            get { return MACRO; }
+        }
+
+        public static string Image
+        {
+            get { return IMAGE; }
+        }
+
+        public static string HtmlBlock
+        {
+            get { return HTML_BLOCK; }
+        }
+
+        public static string Html
+        {
+            get { return HTML; }
+        }
+
+        public static string InlineStyle
+        {
+            get { return INLINE; }
+        }
+
+        public static string InlineStyleProp
+        {
+            get { return INLINE_STYLE_PROP; }
+        }
+
+        public static string Mono
+        {
+            get { return MONO; }
+        }
+
+        public static string Comment
+        {
+            get { return COMMENT; }
+        }
+
+        public static string Combined
+        {
+            get
+            {
+                return "(" + String.Join(")|(.", new string[]{Link, Macro, Image, HtmlBlock, Html, InlineStyle, Mono, Comment, @"''|\/\/|__|\^\^|~~|=="}) + ")";
+            }
+        }
+
+        public static string MacroParamsVar
+        {
+            get { return MACRO_PARAMS_VAR; }
+        }
+
+        public static string MacroParamsFunc
+        {
+            get { return MACRO_PARAMS_FUNC; }
+        }
+
+        public static string MacroParams
+        {
+            get { return MACRO_PARAMS; }
+        }
+
+        public static string ImageFilename
+        {
+            get { return IMAGE_FILENAME; }
+        }
+
+        public static string ExternalImageUrl
+        {
+            get { return EXTERNAL_IMAGE_URL; }
+        }
+
+        public static string ExternalImage
+        {
+            get
+            {
+                return Image.Replace(@"([^\[\]\|]+)", ExternalImageUrl);
+            }
+        }
+
+        public static string HtmlImage
+        {
+            get { return HTML_IMAGE; }
+        }
+
+        public static string CssImage
+        {
+            get { return CSS_IMAGE; }
+        }
+    }
+}

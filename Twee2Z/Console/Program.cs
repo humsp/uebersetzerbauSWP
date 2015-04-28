1af75f472d3da98ea74e6ab183c99b9cb7a09d4c
Rene Kloth <r.kloth@fu-berlin.de>
2015-04-28 21:59:56 +0200

    init lexer

--- Twee2Z/Console/Program.cs
+++ Twee2Z/Console/Program.cs
@@ -5,13 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using Twee2Z.Lexer;
+using System.IO;
 
 namespace Twee2Z.Console
 {
     class Program
     {
+        static string rootFolder = "..\\..\\..\\tweeFiles\\";
+        static string passgaeOnly = rootFolder + "passageOnly.tw";
+        static string passgae = rootFolder + "passage.tw";
+
         static void Main(string[] args)
         {
+            FileStream fs = File.Open(passgaeOnly, FileMode.Open, FileAccess.Read, FileShare.None);
+            StreamReader inputStream = new StreamReader(fs);
+            Lexer.Lex.LexStream(inputStream);
+
+
+            System.Console.Read();
+            /*
             System.Console.WriteLine("Welcome to Twee2Z");
             System.Console.WriteLine("Try tokens like '<<display Page>>', '[[Place]]' or '<html><br></html>'");
             System.Console.WriteLine("");
@@ -38,7 +50,7 @@ namespace Twee2Z.Console
                     System.Console.WriteLine("Comment\n");
                 else
                     System.Console.WriteLine("Unknown token\n");
-            }
+            }*/
         }
     }
 }

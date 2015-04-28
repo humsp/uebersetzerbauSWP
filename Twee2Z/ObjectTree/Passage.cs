db8bf307c5565cf2bae9dff64e463ab85d7bde58
Rene Kloth <r.kloth@fu-berlin.de>
2015-04-28 23:00:23 +0200

    init tree

---
+++ Twee2Z/ObjectTree/Passage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twee2Z.ObjectTree
+{
+    class Passage
+    {
+        public string name;
+        public string[] tags;
+        public PassageNode[] nodes;
+    }
+}

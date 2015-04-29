e73e1877f03446076c76c79754a27e6e57598e8a
Rene Kloth <r.kloth@fu-berlin.de>
2015-04-29 07:28:18 +0200

    improve grammar

---
+++ Twee2Z/Lexer/TweeVisitor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twee2Z.Lexer
+{
+    class TweeVisitor :TweeBaseVisitor<object>
+    {
+
+        int i = 0;
+
+     
+
+        public override object VisitLink(TweeParser.LinkContext context)
+        {
+            Console.WriteLine(i + "_" + context.GetText());
+            i++;
+            return base.VisitLink(context);
+        }
+
+        public override object VisitPassage(TweeParser.PassageContext context)
+        {
+            Console.WriteLine(i + "_" + context.GetText());
+            i++;
+            return base.VisitPassage(context);
+        }
+
+        public override object VisitPassageContent(TweeParser.PassageContentContext context)
+        {
+            Console.WriteLine(i + "_" + context.GetText());
+            i++;
+            return base.VisitPassageContent(context);
+        }
+
+        public override object VisitStart(TweeParser.StartContext context)
+        {
+
+            Console.WriteLine(context.GetType());
+            return base.VisitStart(context);
+        }
+
+
+        
+    }
+}
--- Twee2Z/ObjectTree/PassageNode.cs
+++ Twee2Z/ObjectTree/PassageNode.cs
@@ -8,5 +8,6 @@ namespace Twee2Z.ObjectTree
 {
     class PassageNode
     {
+        
     }
 }

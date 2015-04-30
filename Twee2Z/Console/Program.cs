213e9eb691c741e6882ef5912b8f70576927b724
Rene Kloth <r.kloth@fu-berlin.de>
2015-04-30 14:22:29 +0200

    refactoring

--- Twee2Z/Console/Program.cs
+++ Twee2Z/Console/Program.cs
@@ -23,34 +23,6 @@ namespace Twee2Z.Console
 
 
             System.Console.Read();
-            /*
-            System.Console.WriteLine("Welcome to Twee2Z");
-            System.Console.WriteLine("Try tokens like '<<display Page>>', '[[Place]]' or '<html><br></html>'");
-            System.Console.WriteLine("");
-            while (true)
-            {
-                System.Console.Write("Please enter a token: ");
-                string input = System.Console.ReadLine();
-
-                if (Regex.IsMatch(input, Pattern.Link))
-                    System.Console.WriteLine("Link\n");
-                else if (Regex.IsMatch(input, Pattern.Macro))
-                    System.Console.WriteLine("Macro\n");
-                else if (Regex.IsMatch(input, Pattern.Image))
-                    System.Console.WriteLine("Image\n");
-                else if (Regex.IsMatch(input, Pattern.HtmlBlock))
-                    System.Console.WriteLine("HtmlBlock\n");
-                else if (Regex.IsMatch(input, Pattern.Html))
-                    System.Console.WriteLine("Html\n");
-                else if (Regex.IsMatch(input, Pattern.InlineStyle))
-                    System.Console.WriteLine("InlineStyle\n");
-                else if (Regex.IsMatch(input, Pattern.Mono))
-                    System.Console.WriteLine("Mono\n");
-                else if (Regex.IsMatch(input, Pattern.Comment))
-                    System.Console.WriteLine("Comment\n");
-                else
-                    System.Console.WriteLine("Unknown token\n");
-            }*/
         }
     }
 }
--- Twee2Z/Lexer/TweeVisitor.cs
+++ Twee2Z/Lexer/TweeVisitor.cs
@@ -32,32 +32,21 @@ namespace Twee2Z.Lexer
             {
                 Console.WriteLine("Text:" + context.GetChild(0).GetText());
             }
-            else
-            {
-             //   Console.WriteLine(context.GetChild(0).GetText());
-            }
             return base.VisitPassageContent(context);
         }
 
         public override object VisitStart(TweeParser.StartContext context)
         {
-
-            //Console.WriteLine("Start:____");
-            //Console.WriteLine(context.GetText());
             return base.VisitStart(context);
         }
 
         public override object VisitTxt(TweeParser.TxtContext context)
         {
-            //Console.WriteLine("Start:____");
-            //Console.WriteLine(context.GetText());
             return base.VisitTxt(context);
         }
 
         public override object VisitText(TweeParser.TextContext context)
         {
-           // Console.WriteLine("Start:____");
-            //Console.WriteLine(context.GetText());
             return base.VisitText(context);
         }
 

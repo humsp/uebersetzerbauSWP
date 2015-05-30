c94f0a99799f72cb2f66f9a095fd3575ac78c649
Rene Kloth <r.kloth@fu-berlin.de>
2015-05-31 00:15:58 +0200

    add logger

--- Twee2Z/Analyzer/TweeVisitor.cs
+++ Twee2Z/Analyzer/TweeVisitor.cs
@@ -11,26 +11,26 @@ namespace Twee2Z.Analyzer
     class TweeVisitor : TweeBaseVisitor<object>
     {
 
-        private Tree _tree = new Tree();
+        private Tree _tree;
         private Passage _currentPassage;
 
         public override object VisitStart(TweeParser.StartContext context)
         {
-            new Tree();
-            Console.WriteLine("[Start]");
+            _tree = new Tree();
+            System.Console.WriteLine("[Start]");
             return base.VisitStart(context);
         }
 
         public override object VisitPassage(TweeParser.PassageContext context)
         {
-            Console.WriteLine("[Passage]");
+            System.Console.WriteLine("[Passage]");
 
             return base.VisitPassage(context);
         }
 
         public override object VisitPassageName(TweeParser.PassageNameContext context)
         {
-            Console.WriteLine("Name: " + context.GetText());
+            System.Console.WriteLine("Name: " + context.GetText());
             _currentPassage = new Passage(context.GetText());
             _tree.AddPassage(_currentPassage);
             return base.VisitPassageName(context);
@@ -38,7 +38,7 @@ namespace Twee2Z.Analyzer
 
         public override object VisitPassageTags(TweeParser.PassageTagsContext context)
         {
-            Console.WriteLine("[Tags]");
+            System.Console.WriteLine("[Tags]");
             return base.VisitPassageTags(context);
         }
 
@@ -48,7 +48,7 @@ namespace Twee2Z.Analyzer
             {
                 string tag = context.GetChild(0).GetText();
 
-                Console.WriteLine("Tag: " + tag);
+                System.Console.WriteLine("Tag: " + tag);
                 _currentPassage.AddTag(tag);
 
             }
@@ -58,18 +58,18 @@ namespace Twee2Z.Analyzer
 
         public override object VisitPassageContent(TweeParser.PassageContentContext context)
         {
-            Console.WriteLine("[PassageContent]");
+            System.Console.WriteLine("[PassageContent]");
             return base.VisitPassageContent(context);
         }
 
         public override object VisitLink(TweeParser.LinkContext context)
         {
-            Console.WriteLine("[Link]");
+            System.Console.WriteLine("[Link]");
             if (context.ChildCount == 6)
             {
                 string passageName = context.GetChild(3).GetText();
-                Console.WriteLine("Ziel: " + passageName);
-                Console.WriteLine("Text: " + context.GetChild(1).GetText());
+                System.Console.WriteLine("Ziel: " + passageName);
+                System.Console.WriteLine("Text: " + context.GetChild(1).GetText());
                 if (context.GetChild(1).GetText() == "")
                 {
                     throw new Exception("passage text empty:" + passageName);
@@ -79,9 +79,9 @@ namespace Twee2Z.Analyzer
             else if (context.ChildCount == 9)
             {
                 string passageName = context.GetChild(3).GetText();
-                Console.WriteLine("Ziel: " + passageName);
-                Console.WriteLine("Text: " + context.GetChild(1).GetText());
-                Console.WriteLine("Expression: " + context.GetChild(6).GetText());
+                System.Console.WriteLine("Ziel: " + passageName);
+                System.Console.WriteLine("Text: " + context.GetChild(1).GetText());
+                System.Console.WriteLine("Expression: " + context.GetChild(6).GetText());
                 if (context.GetChild(1).GetText() == "")
                 {
                     throw new Exception("passage text empty:" + passageName);
@@ -90,7 +90,7 @@ namespace Twee2Z.Analyzer
             }
             else
             {
-                Console.WriteLine("Ziel: " + context.GetChild(1).GetText());
+                System.Console.WriteLine("Ziel: " + context.GetChild(1).GetText());
                 _currentPassage.AddPassageContent(new PassageLink(context.GetChild(1).GetText()));
             }
             return base.VisitLink(context);
@@ -98,26 +98,26 @@ namespace Twee2Z.Analyzer
 
         public override object VisitText(TweeParser.TextContext context)
         {
-            Console.WriteLine("Text: " + context.GetText());
+            System.Console.WriteLine("Text: " + context.GetText());
             _currentPassage.AddPassageContent(new PassageText(context.GetText()));
             return base.VisitText(context);
         }
 
         public override object VisitVariable(TweeParser.VariableContext context)
         {
-            Console.WriteLine("Variable: " + context.GetText());
+            System.Console.WriteLine("Variable: " + context.GetText());
             _currentPassage.AddPassageContent(new PassageVariable(context.GetText(), 0));
             //_tree.SetVariable(new Variable(context.GetText()));
             return base.VisitVariable(context);
         }
         public override object VisitFunction(TweeParser.FunctionContext context)
         {
-            Console.WriteLine("Function: " + context.GetText());
+            System.Console.WriteLine("Function: " + context.GetText());
             return base.VisitFunction(context);
         }
         public override object VisitMacro(TweeParser.MacroContext context)
         {
-            Console.WriteLine("Macro: " + context.GetText());
+            System.Console.WriteLine("Macro: " + context.GetText());
             return base.VisitMacro(context);
         }
 

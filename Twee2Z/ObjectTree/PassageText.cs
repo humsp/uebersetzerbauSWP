48d2f2e995926d253ddbd40ad8c34cfb61d167ef
Majd Hasan <majd.hasan@hotmail.com>
2015-06-10 14:10:54 +0200

    Format in ObjectTree

--- Twee2Z/ObjectTree/PassageText.cs
+++ Twee2Z/ObjectTree/PassageText.cs
@@ -9,6 +9,23 @@ namespace Twee2Z.ObjectTree
     public class PassageText : PassageContent
     {
         private string _text;
+        private bool _italic;
+        private bool _underline;
+        private bool _strikeout;
+        private bool _subscript;
+        private bool _monospae;
+        private bool _comment;
+
+        /*
+: (ITALIC_BEGIN	(ITALIC_TEXT_SWITCH | ITALIC_TEXT | passageContent)* ITALIC_END)
+| (UNDERLINE_BEGIN (UNDERLINE_TEXT_SWITCH | UNDERLINE_TEXT | passageContent)* UNDERLINE_END)
+| (STRIKEOUT_BEGIN (STRIKEOUT_TEXT_SWITCH | STRIKEOUT_TEXT | passageContent)*	STRIKEOUT_END)	
+| (SUPERSCRIPT_BEGIN (SUPERSCRIPT_TEXT_SWITCH | SUPERSCRIPT_TEXT | passageContent)* SUPERSCRIPT_END)
+| (SUBSCRIPT_BEGIN (SUBSCRIPT_TEXT_SWITCH | SUBSCRIPT_TEXT | passageContent)* SUBSCRIPT_END)
+| (MONOSPACE_BEGIN (MONOSPACE_TEXT_SWITCH | MONOSPACE_TEXT | passageContent)* MONOSPACE_END)
+| (COMMENT_BEGIN (COMMENT_TEXT_SWITCH | COMMENT_TEXT | passageContent)* COMMENT_END)
+;
+ */
 
         public PassageText(string text)
             : base(ContentType.TextContent)
@@ -23,5 +40,8 @@ namespace Twee2Z.ObjectTree
                 return _text;
             }
         }
+
+
+
     }
 }

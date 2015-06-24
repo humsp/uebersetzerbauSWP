118c9e8c87a792dfe2c79a73e15ad4badff70ab3
aliagbaria <bo3el.fa7m@hotmail.com>
2015-06-24 15:39:39 +0200

    Funktionen

--- Twee2Z/ObjectTree/PassageFunction.cs
+++ Twee2Z/ObjectTree/PassageFunction.cs
@@ -4,54 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Twee2Z.ObjectTree
+namespace Twee2Z.ObjectTree 
 {
-    public class PassageFunction : PassageContent
-    {
-        private string _functionName;
-        private List<string> _args;
-        /*
-         * _functionName = context.GetChild(0).GetText();
-            String _paramList
-         * */
+	class PassageFunction : PassageContent
+	{
 
-        public PassageFunction(string func)
-            : base(ContentType.FunctionContent)
-        {
-            _func = func;
-        }
-
-        public void addArg(string argument)
-        {
-            if (!_args.Contains(argument))
-            {
-                _args.Add(argument);
-            }
-        }
+		private string _functionName;
+		private string _paramList ;
 
 
-        public string Function
-        {
-            get
-            {
-                return _func;
-            }
-            set
-            {
-                _func = value;
-            }
-        }
 
-        public List<string> Target
-        {
-            get
-            {
-                return _args;
-            }
-            set
-            {
-                _args = value;
-            }
-        }
-    }
+
+		public PassageFunction(string functionName, string paramList)
+			: base(ContentType.FunctionContent)
+		{
+			_functionName = functionName;
+			_paramList = paramList;
+		 	
+		}
+
+
+
+		public string FuncName
+		{
+			get
+			{
+				return _functionName;
+			}
+			set
+			{
+				_functionName = value;
+			}
+		}
+
+		public string paramList
+		{
+			get
+			{
+				return _paramList;
+			}
+			set
+			{
+				_paramList = value;
+			}
+		}
+
+	}
 }
